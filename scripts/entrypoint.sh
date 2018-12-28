d0db1cc037e6e87bc5a1fa4aaa1d0f5ccfbd81f9
iromli <isman.firmansyah@gmail.com>
2018-12-28 23:29:02 +0700

    fix typo

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -57,7 +57,7 @@ if [ ! -f /deploy/touched ]; then
         mv /touched /deploy/touched
     else
         if [ -f /etc/redhat-release ]; then
-            source scl_source enable ptyhon27 && python /opt/scripts/wait_for.py --deps="config,secret" && python /opt/scripts/entrypoint.py
+            source scl_source enable python27 && python /opt/scripts/wait_for.py --deps="config,secret" && python /opt/scripts/entrypoint.py
         else
             python /opt/scripts/wait_for.py --deps="config,secret" && python /opt/scripts/entrypoint.py
         fi
